58b0220ece9ac809fb464daad502d78195440a11
aruniiird <arun.iiird@gmail.com>
2019-08-15 19:50:42 +0530

    Removed the hard coded profile name
    Added flexibility to create your own profile
    Added command line arguments and help
    
    Signed-off-by: aruniiird <arun.iiird@gmail.com>

--- start_demo_setup.sh
+++ start_demo_setup.sh
@@ -2,14 +2,14 @@
 
 DIR_0="$(cd $(dirname $0) && pwd)"
 ENV_F="$DIR_0/minikube.env"
-PROFILE_NAME="storage_event_management"
 # the below order of files should not be changed
 NEEDED_FILES=(common.yaml operator.yaml cluster.yaml toolbox.yaml pool.yaml filesystem.yaml)
 MINIKUBE_DATA_DIR="/data/rook"
 STORAGE_BASE_DIR="$PWD/libvirt"
+unset TOBE_CLEANED
 
 function needed_binaries() {
-  local binary_list=(base64 minikube qemu-img virsh kubectl scp wget)
+  local binary_list=(base64 minikube qemu-img virsh kubectl scp wget awk)
   local not_found=""
   for eachB in ${binary_list[@]};do
     if ! which $eachB 2>/dev/null 1>&2;then
@@ -19,7 +19,6 @@ function needed_binaries() {
     fi
   done
   [ -n "$not_found" ] && return 1
-  [ -z "$BASE64_SUDO_PASS" ] && echo -e "\nA base64 encrypted sudo password is required to run this script...\nPass it through environment variable 'BASE64_SUDO_PASS'\nEg: echo '<YOUR_SUDO_PASSWORD>' |base64" && return 1
   return 0
 }
 
@@ -27,10 +26,10 @@ function start_minikube() {
   echo
   minikube config view
   echo
-  if minikube status -p $PROFILE_NAME >/dev/null;then
+  if minikube status $PROFILE_NAME_WITH_P >/dev/null;then
     echo -e "\nStorage-Event-Management minikube cluster is running..."
   else
-    minikube start -p $PROFILE_NAME
+    minikube start $PROFILE_NAME_WITH_P
   fi
   return $?
 }
@@ -42,12 +41,13 @@ function create_virtual_disks() {
   [ -z "${nDisks}" ] && nDisks=3
   local diskLocation="$STORAGE_BASE_DIR/images"
   [ ! -d "${diskLocation}" ] && mkdir -p "${diskLocation}"
+  local profName=$PROFILE_NAME && [ -z "$profName" ] && profName="minikube"
   for (( i=1; i<=${nDisks}; i++ ));do
     local disk="${diskLocation}/osd$i.qcow2"
     [ ! -f ${disk} ] && qemu-img create -f qcow2 ${disk} 1T
-    echo "$BASE64_SUDO_PASS" |base64 --decode |sudo -S virsh attach-disk $PROFILE_NAME ${disk} vd${appendAlpha[i]} --driver qemu --subdriver qcow2 --targetbus virtio --persistent
+    echo "$BASE64_SUDO_PASS" |base64 --decode |sudo -S virsh attach-disk $profName ${disk} vd${appendAlpha[i]} --driver qemu --subdriver qcow2 --targetbus virtio --persistent
   done
-  minikube -p $PROFILE_NAME ssh "echo 'echo 1 >/sys/bus/pci/rescan' |su -"
+  minikube $PROFILE_NAME_WITH_P ssh "echo 'echo 1 >/sys/bus/pci/rescan' |su -"
 }
 
 function edit_yaml_files() {
@@ -99,41 +99,97 @@ function setup_rook() {
   done
 }
 
-function setup_test_framework() {
-  [ ! -f module.py ] && echo "The file, 'module.py', exists..." &&  wget https://raw.githubusercontent.com/pcuzner/ceph/add-events-mgr-module/src/pybind/mgr/k8sevents/module.py
-  [ ! -f module.py ] && echo "Unable to fetch 'module.py' file..." && return 1
-  scp -o StrictHostKeyChecking=no -i $MINIKUBE_HOME/.minikube/machines/$PROFILE_NAME/id_rsa $PWD/module.py docker@$(minikube -p $PROFILE_NAME ip):~/. || { echo "Unable to copy the 'module.py' to minikube cluster..."; return 1; }
-  minikube -p $PROFILE_NAME ssh "echo 'mkdir -p $MINIKUBE_DATA_DIR/rook-ceph/log/' |su -"
-  minikube -p $PROFILE_NAME ssh "echo 'cp -f ~docker/module.py $MINIKUBE_DATA_DIR/rook-ceph/log/module.py' |su -"
-  local mgrPod=""
+function get_pod_with_state() {
+  local podSearchLine="$1" && [ -z "$podSearchLine" ] && echo "Sorry, was expecting atleast a pod name" >&2 && return 1
+  local expectedState="$2" && [ -z "$expectedState" ] && expectedState="Running"
+  local iterateTill="$3" && [ -z "$iterateTill" ] && iterateTill=20
+  local foundPod=""
   local i=0
-  while [ -z "$mgrPod" ] && [ $i -lt 20 ];do
-    mgrPod="$(kubectl get pods -n rook-ceph |grep "mgr" |sed -n 's/^\([^ ]*\).*/\1/gp')"
+  while [ -z "${foundPod}" ] && [ $i -lt $iterateTill ];do
+    local podLine="$(kubectl get pods -n rook-ceph |grep "${podSearchLine}")"
+    if [ -n "${podLine}" ];then
+      local isInState="$(echo "${podLine}" |grep "\<${expectedState}\>")"
+      [ -n "${isInState}" ] && foundPod="$(echo "${podLine}" |awk '{print $1}')"
+    fi
     (( i++ ))
     sleep "$(( i * 10))s"
-    kubectl get pods -n rook-ceph
+    kubectl get pods -n rook-ceph >&2
   done
+  [ -z "${foundPod}" ] && echo "Unable to find manager pod..." >&2 && return 1
+  echo "${foundPod}"
+}
+
+function setup_test_framework() {
+  # [ ! -f module.py ] && echo "The file, 'module.py', doesn't exists..." &&  wget https://raw.githubusercontent.com/pcuzner/ceph/add-events-mgr-module/src/pybind/mgr/k8sevents/module.py
+  [ ! -f module.py ] && echo "The file, 'module.py', doesn't exists..." &&  wget https://raw.githubusercontent.com/ceph/ceph/a6cf6c1abdeba6d412b68315966b398ce8ad01d9/src/pybind/mgr/k8sevents/module.py
+  [ ! -f module.py ] && echo "Unable to fetch 'module.py' file..." && return 1
+  local profName=$PROFILE_NAME && [ -z "$profName" ] && profName="minikube"
+  scp -o StrictHostKeyChecking=no -i $MINIKUBE_HOME/.minikube/machines/$profName/id_rsa $PWD/module.py docker@$(minikube $PROFILE_NAME_WITH_P ip):~/. || { echo "Unable to copy the 'module.py' to minikube cluster..."; return 1; }
+  minikube $PROFILE_NAME_WITH_P ssh "echo 'mkdir -p $MINIKUBE_DATA_DIR/rook-ceph/log/' |su -"
+  minikube $PROFILE_NAME_WITH_P ssh "echo 'cp -f ~docker/module.py $MINIKUBE_DATA_DIR/rook-ceph/log/module.py' |su -"
+  local mgrPod="$(get_pod_with_state "mgr" "Running")"
   [ -z "${mgrPod}" ] && echo "Unable to find manager pod..." && return 1
   local k8sEventsDir="/usr/share/ceph/mgr/k8sevents"
-  echo "mkdir -p $k8sEventsDir" |kubectl -n rook-ceph exec -it ${mgrPod} sh 2>/dev/null
-  echo "echo 'from .module import Module' >${k8sEventsDir}/__init__.py" |kubectl -n rook-ceph exec -it ${mgrPod} sh 2>/dev/null
-  echo "cp /var/log/ceph/module.py ${k8sEventsDir}" |kubectl -n rook-ceph exec -it ${mgrPod} sh 2>/dev/null
-  local toolsPod="$(kubectl get pods -n rook-ceph |grep "tools" |sed -n 's/^\([^ ]*\).*/\1/gp')"
-  [ -z "$toolsPod" ] && echo "Unable to find toolbox pod..." && return 1
-  echo "ceph mgr module enable k8events --force" |kubectl -n rook-ceph exec -it ${toolsPod} sh 2>/dev/null
+  echo "mkdir -p $k8sEventsDir" |kubectl -n rook-ceph exec -it ${mgrPod} sh # 2>/dev/null
+  echo "echo 'from .module import Module' >${k8sEventsDir}/__init__.py" |kubectl -n rook-ceph exec -it ${mgrPod} sh #2>/dev/null
+  echo "cp /var/log/ceph/module.py ${k8sEventsDir}" |kubectl -n rook-ceph exec -it ${mgrPod} sh # 2>/dev/null
+  local toolsPod="$(get_pod_with_state "tools" "Running")"
+  [ -z "$toolsPod" ] && echo "Unable to find toolbox pod..." >&2 && return 1
+  echo "ceph mgr module enable k8events --force" |kubectl -n rook-ceph exec -it ${toolsPod} sh #2>/dev/null
 }
 
 function cleanup() {
-  if minikube status -p $PROFILE_NAME >/dev/null;then
-    minikube stop -p $PROFILE_NAME
+  if minikube status $PROFILE_NAME_WITH_P >/dev/null;then
+    minikube stop $PROFILE_NAME_WITH_P
   fi
-  echo "$BASE64_SUDO_PASS" |base64 --decode |sudo -S virsh undefine $PROFILE_NAME --remove-all-storage --delete-snapshots
-  minikube delete -p $PROFILE_NAME
-  rm -rf $MINIKUBE_HOME/.minikube/machines/$PROFILE_NAME
-  rm -rf $MINIKUBE_HOME/.minikube/profiles/$PROFILE_NAME
+  local profName=$PROFILE_NAME && [ -z "$profName" ] && profName="minikube"
+  echo "$BASE64_SUDO_PASS" |base64 --decode |sudo -S virsh list --all
+  echo "Removing $profName virtual storage"
+  echo "$BASE64_SUDO_PASS" |base64 --decode |sudo -S virsh undefine $profName --remove-all-storage --delete-snapshots
+  minikube delete $PROFILE_NAME_WITH_P
+  rm -rf $MINIKUBE_HOME/.minikube/machines/$profName
+  rm -rf $MINIKUBE_HOME/.minikube/profiles/$profName
   rm -rf $STORAGE_BASE_DIR
+  [ -f "${MINIKUBE_CONFIG_DIR}/config.json.bak" ] && mv ${MINIKUBE_CONFIG_DIR}/config.json.bak ${MINIKUBE_CONFIG_DIR}/config.json
   return 0
 }
+
+function help() {
+  echo "Commandline Arguments"
+  echo "---------------------"
+  echo "-p <PROFILE_NAME>                 : argument will be used as profile to start the minikube instance"
+  echo "-bp <BASE64_ENCODED_SUDO_PASSWORD>: base64 encoded sudo password"
+  echo "-h | --help | -help               : prints this help"
+  echo "-h | --help | -help               : prints this help"
+  echo "clean | cleanup                   : cleanup any previously created setup"
+  echo ""
+  echo "Environment variables"
+  echo "---------------------"
+  echo "BASE64_SUDO_PASS : [mandatory] base64 encoded 'sudo' password."
+  echo "                 : eg: BASE64_SUDO_PASS=\"\$(echo <your_sudo_password> |base64)\""
+  echo "PROFILE_NAME     : [optional] will provide the profile name that should be used for creating minikube"
+  echo "                 : (default: minikube)"
+  echo "ROOK_GITHUB_HOME : github cloned location of the rook project. If not provided, will clone into current directory."
+  echo "                 : (default: \$PWD/rook)"
+}
+
+function parse_command_line_args() {
+  if echo "$@" | grep "\-h" >/dev/null;then
+    help
+    exit 0
+  fi
+  local profName="$(echo "$@" |sed -n 's@.*-p[[:space:]]\+\([^ ]*\).*@\1@gp')"
+  [ -n "$profName" ] && PROFILE_NAME="$profName"
+  # if PROFILE_NAME is set (through above line or environment variable)
+  [ -n "$PROFILE_NAME" ] && PROFILE_NAME_WITH_P="-p $PROFILE_NAME"
+  local b64SudoPass="$(echo "$@" |sed -n 's@.*-bp[[:space:]]\+\([^ ]*\).*@\1@gp')"
+  [ -n "$b64SudoPass" ] && BASE64_SUDO_PASS="$b64SudoPass"
+  [ -z "$BASE64_SUDO_PASS" ] && echo -e "\nA base64 encrypted sudo password is required to run this script...\nPass it through environment variable 'BASE64_SUDO_PASS'\nOr\nUse '-bp <BASE64_SUDO_PASS>' in the commandline\nTo generate BASE64_SUDO_PASS:\necho '<YOUR_SUDO_PASSWORD>' |base64" >&2 && exit 1
+  echo $BASE64_SUDO_PASS |base64 --decode 1>/dev/null 2>&1 || { echo "Invalide base64 password" >&2; exit 1; }
+  echo "$@" |grep "\<clean\>\|\<cleanup\>" 1>/dev/null 2>&1 && TOBE_CLEANED="true"
+}
+
+parse_command_line_args $@
 
 if ! needed_binaries;then
   exit 1
@@ -142,7 +198,7 @@ fi
 [ ! -f "$ENV_F" ] && echo "No environment file found! Sorry!" && exit 1
 source $DIR_0/minikube.env
 
-if [ "$1" = "clean" ];then
+if [ -n "$TOBE_CLEANED" ];then
   cleanup
   exit $?
 fi
